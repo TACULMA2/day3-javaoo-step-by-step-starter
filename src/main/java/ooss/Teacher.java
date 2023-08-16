7690fc775825b3926de338b69fbd55a0b6937b30
TACULMA2 <marigold.taculog@oocl.com>
2023-08-16 18:56:02 +0800

    test: test passed from step4 TeacherTest should_return_result_by_is_belong_to_when_belongsTo_given_teacher_teaches_in_the_class()

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -4,6 +4,7 @@ public class Teacher extends Person {
     private final int age;
     private final String name;
     private final int id;
+    private Klass assignedClass;
     public Teacher(int id, String name, int age) {
         super(id, name, age);
         this.name = name;
@@ -14,5 +15,13 @@ public class Teacher extends Person {
     @Override
     public String introduce() {
         return super.introduce() + " I am a teacher.";
+    }
+
+    public void assignTo(Klass klass) {
+        assignedClass = klass;
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return assignedClass != null && assignedClass.equals(klass);
     }
 }
