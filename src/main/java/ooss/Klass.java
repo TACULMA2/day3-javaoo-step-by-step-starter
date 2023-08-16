7dd90dc99e6bc3379cff88ef47d728eb8b53e65b
TACULMA2 <marigold.taculog@oocl.com>
2023-08-16 18:34:37 +0800

    feat: add the test from step3 StudentTest should_return_result_by_student_is_in_class_when_isIn_given_student()

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,5 +1,24 @@
 package ooss;
 
-public class Klass {
+import java.util.Objects;
 
+public class Klass {
+    private int classId;
+
+    public Klass(int classId) {
+        this.classId = classId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return classId == klass.classId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(classId);
+    }
 }
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -4,6 +4,7 @@ public class Student extends Person {
     private final int age;
     private final String name;
     private final int id;
+    private Klass enrolledClass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
@@ -15,5 +16,13 @@ public class Student extends Person {
     @Override
     public String introduce() {
         return super.introduce() + " I am a student.";
+    }
+
+    public void join(Klass klass) {
+        enrolledClass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return enrolledClass != null && enrolledClass.equals(klass);
     }
 }
